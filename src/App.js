8ec6c84c50100e8490005a7e6fb8302a3e2d4acf
rafaellore <birdneon3@gmail.com>
2022-05-30 09:46:23 -0300

    feat(components): adds the initial components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login/Login';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
