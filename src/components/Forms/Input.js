bed51d880dc9a2e0d96827aff9f884b194501539
rafaellore <birdneon3@gmail.com>
2022-05-31 17:46:30 -0300

    feat(form): adds the useForm hook

--- src/components/Forms/Input.js
+++ src/components/Forms/Input.js
@@ -2,14 +2,22 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-export const Input = ({ label, type, name }) => {
+export const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} name={name} className={styles.input} type={type} />
-      <p className={styles.error}>Error</p>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
