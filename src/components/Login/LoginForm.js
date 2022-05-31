880b733ce76dd51826a8729273ceec0e2ff555ba
rafaellore <birdneon3@gmail.com>
2022-05-31 16:20:23 -0300

    feat(form): adds button and input components

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../Forms/Button';
+import { Input } from '../Forms/Input';
 
 function LoginForm() {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  // const [username, setUsername] = React.useState('');
+  // const [password, setPassword] = React.useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+  function handleSubmit() {
+    // event.preventDefault();
+    // fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ username, password }),
+    // })
+    //   .then((response) => response.json())
+    //   .then((json) => console.log(json));
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
-        <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} />
-        <button>Entrar</button>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
