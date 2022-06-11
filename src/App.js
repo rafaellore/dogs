70fb7ab8a0419235ad954ce597af93b06c9ad944
rafaellore <birdneon3@gmail.com>
2022-06-11 15:04:18 -0300

    feat(UserContext): adds UserContext with requisitions

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
 import Login from './components/Login/Login';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login/*" element={<Login />} />
-      </Routes>
-      <Footer />
+      <UserStorage>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </UserStorage>
     </BrowserRouter>
   );
 }
