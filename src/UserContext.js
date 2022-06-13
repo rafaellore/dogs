713f7a8b06e94bdbc3e2ec0a42d410477db1f53a
rafaellore <birdneon3@gmail.com>
2022-06-13 10:45:41 -0300

    feat(UserContext): adds functions to validate token

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { TOKEN_POST, USER_GET } from './api';
+import { useNavigate } from 'react-router-dom';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
 
 export const UserContext = React.createContext();
 
@@ -9,6 +10,47 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+
+  const navigate = useNavigate();
+
+  const userLogout = React.useCallback(
+    async function userLogout() {
+      setData(null);
+      setError(null);
+      setLoading(false);
+      setLogin(false);
+      window.localStorage.removeItem('token');
+      navigate('/login');
+    },
+    [navigate],
+  );
+
+  React.useEffect(() => {
+    async function autoLogin() {
+      const token = window.localStorage.getItem('token');
+
+      if (token) {
+        try {
+          setError(null);
+          setLoading(true);
+
+          const { url, options } = TOKEN_VALIDATE_POST(token);
+
+          const response = await fetch(url, options);
+          if (!response.ok) throw new Error('Invalid Token');
+
+          await getUser(token);
+        } catch (err) {
+          userLogout();
+
+          console.log(err);
+        } finally {
+          setLoading(false);
+        }
+      }
+    }
+    autoLogin();
+  }, [userLogout]);
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
@@ -19,12 +61,31 @@ export const UserStorage = ({ children }) => {
   }
 
   async function userLogin(username, password) {
-    const { url, options } = TOKEN_POST({ username, password });
-    const tokenRes = await fetch(url, options);
-    const { token } = await tokenRes.json();
-    window.localStorage.setItem('token', token);
-    getUser(token);
+    try {
+      setError(null);
+      setLoading(true);
+
+      const { url, options } = TOKEN_POST({ username, password });
+      const tokenRes = await fetch(url, options);
+
+      if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`);
+
+      const { token } = await tokenRes.json();
+      window.localStorage.setItem('token', token);
+
+      await getUser(token);
+      navigate('/conta');
+    } catch (err) {
+      setError(err.message);
+      setLogin(false);
+    } finally {
+      setLoading(false);
+    }
   }
 
-  return <UserContext.Provider value={{ userLogin, data }}>{children}</UserContext.Provider>;
+  return (
+    <UserContext.Provider value={{ userLogin, userLogout, data, error, loading, login }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+
 import LoginCreate from './LoginCreate';
 import LoginForm from './LoginForm';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 
 function Login() {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
   return (
     <div>
       <Routes>
