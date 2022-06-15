b18695bdc5902984dbaf600d2aa497991e0ce973
rafaellore <birdneon3@gmail.com>
2022-06-15 13:01:02 -0300

    refactor(LoginForm): improves LoginForm with new styles and Error component

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { TOKEN_POST, USER_GET } from '../../api';
 import { useForm } from '../../hooks/useForm';
 import { UserContext } from '../../UserContext';
 import Button from '../Forms/Button';
 import { Input } from '../Forms/Input';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../Forms/Button.module.css';
 
 function LoginForm() {
   const username = useForm();
@@ -21,15 +23,30 @@ function LoginForm() {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
+
         {loading ? <Button disabled>Carregando ...</Button> : <Button>Entrar</Button>}
-        {error && <p>{error}</p>}
+
+        <Error error={error} />
       </form>
-      <Link to="/login/criar">Cadastro</Link>
+
+      <Link className={styles.perdeu} to="/login/perdeu">
+        Perdeu a senha?
+      </Link>
+
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+
+        <Link className={stylesBtn.button} to="/login/criar">
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 }
