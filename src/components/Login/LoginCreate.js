8d9081d1a97f0c0a6832b961c73f9ac01ab123f4
rafaellore <birdneon3@gmail.com>
2022-06-17 16:49:39 -0300

    refactor(LoginCreate): improves LoginCreate with async function

--- src/components/Login/LoginCreate.js
+++ src/components/Login/LoginCreate.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { USER_POST } from '../../api';
+import { useForm } from '../../hooks/useForm';
+import { UserContext } from '../../UserContext';
+import Button from '../Forms/Button';
+import { Input } from '../Forms/Input';
 
 function LoginCreate() {
-  return <div>LoginCreate</div>;
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit() {
+    event.preventDefault();
+
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+
+    const response = await fetch(url, options);
+
+    if (response.ok) userLogin(username.value, password.value);
+    console.log(response);
+  }
+
+  return (
+    <section>
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  );
 }
 
 export default LoginCreate;
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    message:
+      'Preencha uma senha válida, contendo no minímo 8 caracteres, uma letra maiuscula, uma minuscula, um numero e um caractere especial.',
+  },
 };
 
 export const useForm = (type) => {
